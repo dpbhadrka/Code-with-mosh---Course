ae0657f943e32602a40572311694202ab8b5f1c0
dpbhadrka <p.bhadrka7@gecg28.ac.in>
2024-10-27 21:35:52 +0530

    Component updated with props.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import ListGroup from './Components/ListGroup/ListGroup'
 
 function App() {
-
+const fruits = ["Apple", "Banana", "Pineapple", "Grapes", "Guava", "Kiwi"];
+const names = ["A", "B", "C", "D", "E", "F"];
   return (
     <div className="App">
-    <ListGroup/>
+      <ListGroup items={fruits} title="Fruits"/>
+      <ListGroup items={names} title="Name"/>
     </div>
   )
 }
--- src/Components/ListGroup/ListGroup.tsx
+++ src/Components/ListGroup/ListGroup.tsx
@@ -1,18 +1,22 @@
-import {MouseEvent, useState} from 'react'
+import { MouseEvent, useState } from 'react'
+interface ListGroupProps {
+    items: string[],
+    title:string
+}
 
-function ListGroup() {
-    const fruits = ["Apple", "Banana", "Pineapple", "Grapes", "Guava", "Kiwi"];
+function ListGroup({items,title}:ListGroupProps) {
+    
     // const fruits:string[] = [];
     const [selectedIndex, setIndex] = useState(0);
 
     const handleClick = (event: MouseEvent) => console.log(event.target);
     return (
     <div className='m-auto w-50'>
-        <h1>List</h1>
+            <h1>{title}</h1>
             <ul className="list-group">
-                {fruits.length === 0 && <li className="list-group-item">Not item found.</li>}
+                {items.length === 0 && <li className="list-group-item">Not item found.</li>}
                 
-        {fruits.map((item,index) => <li className={selectedIndex==index ? "list-group-item active" : "list-group-item"} onClick={()=> setIndex(index)} key={item}>{item}</li>)}
+        {items.map((item,index) => <li className={selectedIndex==index ? "list-group-item active" : "list-group-item"} onClick={()=> setIndex(index)} key={item}>{item}</li>)}
         </ul>
     </div>
     )
