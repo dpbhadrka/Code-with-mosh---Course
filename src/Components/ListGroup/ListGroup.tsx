31e171a169f04b7aa4a59ab5f883e9aaea9e9034
dpbhadrka <p.bhadrka7@gecg28.ac.in>
2024-10-27 21:11:59 +0530

    Added state for active list item.

--- src/Components/ListGroup/ListGroup.tsx
+++ src/Components/ListGroup/ListGroup.tsx
@@ -1,19 +1,20 @@
-import {MouseEvent} from 'react'
+import {MouseEvent, useState} from 'react'
 
 function ListGroup() {
     const fruits = ["Apple", "Banana", "Pineapple", "Grapes", "Guava", "Kiwi"];
     // const fruits:string[] = [];
+    const [selectedIndex, setIndex] = useState(0);
 
     const handleClick = (event: MouseEvent) => console.log(event.target);
     return (
-    <>
+    <div className='m-auto w-50'>
         <h1>List</h1>
             <ul className="list-group">
                 {fruits.length === 0 && <li className="list-group-item">Not item found.</li>}
                 
-        {fruits.map(item => <li className="list-group-item active" onClick={(event) =>handleClick(event)} key={item}>{item}</li>)}
+        {fruits.map((item,index) => <li className={selectedIndex==index ? "list-group-item active" : "list-group-item"} onClick={()=> setIndex(index)} key={item}>{item}</li>)}
         </ul>
-    </>
+    </div>
     )
 }
 
